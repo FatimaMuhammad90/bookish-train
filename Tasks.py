44f1e5f5bc75d8eb77e09f06ed0e035ef3b96c9d
Fatima Muhammad <your-email@example.com>
2026-07-21 00:47:19 +0500

    Create operation

--- Tasks.py
+++ Tasks.py
@@ -1,29 +1,46 @@
-import datetime
 
 from fastapi import FastAPI, HTTPException 
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Optional
 
-class Task(BaseModel):
-    name: str
-    date: datetime  
+class TaskCreate(BaseModel):
+  title: str = Field(..., min_length=1)
+
+class TaskUpdate(BaseModel):
+  title: Optional[str] = Field(None, min_length=1)
+  done: Optional[bool] = None
+
 app = FastAPI()
 
 tasks = {}
+count = 0
 
-
-@app.post("/")
-def create(task: Task):
-    name = task.name
-    if name in tasks:
+@app.post("/tasks")
+def create(task: TaskCreate):
+    global count
+    count += 1
+    id = count
+    title = task.title
+    if id in tasks:
         raise HTTPException(status_code=409, detail="Task exists")
-    tasks[name] = task
-    return {"message": f"Added {name} to tasks."}
+    tasks[id] = {"title": title, "done": False}
+    return {"message": f"Added {title} to tasks."}
  
-@app.put("/{name}")
-def update(name: str, task: Task):
-    if name not in tasks:
+@app.put("/tasks/{id}")
+def update(id: int, task: TaskUpdate):
+    if id not in tasks:
          raise HTTPException(status_code=404, detail="Task not found")
-    tasks[name] = task
-    return {"message": f"Updated {name}."}
+    if task.title is not None:
+        tasks[id]["title"] = task.title
+    if task.done is not None:
+        tasks[id]["done"] = task.done
+    return {"message": f"Updated task with ID {id}."}
+
+
+@app.delete("/tasks/{id}")
+def delete(id: int):
+    if id not in tasks:
+         raise HTTPException(status_code=404, detail="Task not found")
+    del tasks[id]
+    return {}
 
