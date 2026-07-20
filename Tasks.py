2e06e56bdbdbae244a48ea7dd898a5e6c642813a
Fatima Muhammad <your-email@example.com>
2026-07-21 00:17:00 +0500

    Update operation created

--- Tasks.py
+++ Tasks.py
@@ -20,3 +20,10 @@ def create(task: Task):
     tasks[name] = task
     return {"message": f"Added {name} to tasks."}
  
+@app.put("/{name}")
+def update(name: str, task: Task):
+    if name not in tasks:
+         raise HTTPException(status_code=404, detail="Task not found")
+    tasks[name] = task
+    return {"message": f"Updated {name}."}
+
